8077a7678b09c5cdd48fad19495529516ce07d2e
Pat Marshall <pmarshall1716@gmail.com>
2016-08-10 16:44:27 -0600

    form creation and csv submission

--- php_project.php
+++ php_project.php
@@ -1,8 +1,31 @@
 <?php 
-
-echo "Welcome to the Prestman Auto Search Form" ;
-
+	echo "Welcome to the Prestman Auto Search Form" ;
 ?>
+
+<hr/>
+
+<?php
+	echo "Enter Name, e-mail address, and select file to upload here for inventory information:";
+?>
+
+	<!--variables-->
+
+  <form action="/" method="POST" enctype="multipart/form-data">
+	 <p>Your name: <input type="text" name="name" placeholder="name here" /></p>
+	 <p>e-mail address: <input type="text" name="age" placeholder="e-mail here" /></p>
+	 <p>Choose File:</p>
+    <input type="file" name="upload" />
+    <input type="submit"/>
+  </form>
+      
+   </body>
+</html>
+
+
+
+
+
+
 
 <hr/>
 
