b58a830ba2b42967623664162ba4364934a4609a
Pat Marshall <pmarshall1716@gmail.com>
2016-08-10 18:58:00 -0600

    parsing through rows

--- php_project.php
+++ php_project.php
@@ -10,7 +10,7 @@
 
 	<!--variables-->
 
-  <form action="/" method="POST" enctype="multipart/form-data">
+  <form action="" method="POST" enctype="multipart/form-data">
 	 <p>Your name: <input type="text" name="name" placeholder="name here" /></p>
 	 <p>e-mail address: <input type="text" name="age" placeholder="e-mail here" /></p>
 	 <p>Choose File:</p>
@@ -27,16 +27,20 @@ echo "Data Processed@";
 date_default_timezone_set('MST');
 
 echo date( 'H:i:s l F jS Y' );
-
 ?>
-<br>
+
+<hr/>
 
 <?php
 
 $file = file('Nextgear.csv');
 
-foreach ($file as $k) 
-	$csv[]=explode(',',$k);
+foreach ($file as $line) {
+	// $csv[]=explode(',',$k);
+	echo "$line<br>";
+		list($stock, $description, $Floor_Date, $source) = explode(",", $line);
+		echo "<h1>$stock, $description, $Floor_Date, $source </h1>";
+	}
 
 print_r($file);
 
